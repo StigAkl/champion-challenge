2d6a5a9cc6163b91f4a8fca016ce5ade9ba89df9
stigakl <stigakl@outlook.com>
2023-06-26 17:27:01 +0200

    pie chart

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import compiled from './data/compiled.json';
 import ChampionCard from './Components/ChampionCard/ChampionCard';
+import { PieChart } from 'react-minimal-pie-chart';
 
 export type Result = "win" | "loss";
 export type Position = "top" | "jungle" | "mid" | "bot" | "support";
+
 export interface Champion {
   id: string;
   name: string;
@@ -12,6 +14,7 @@ export interface Champion {
   result?: Result;
   position?: Position;
 }
+
 
 const App = () => {
   const [search, setSearch] = useState('');
@@ -40,10 +43,34 @@ const App = () => {
     />
   ));
 
+  const pieData = getPiechartData(champions);
   return (
     <>
       <h1>Champion Challenge</h1>
       <p>Antall spilt: {playerCounter} / {compiled.length}</p>
+      <div className="positionDiagram">
+        <PieChart
+          data={pieData}
+          radius={30}
+          lineWidth={100}
+          label={({ dataEntry }) => dataEntry.title}
+          labelStyle={(index) => {
+            if (pieData[index].value > 0) {
+              return {
+                fontSize: '5px',
+                fontFamily: 'sans-serif',
+              }
+            } else {
+              return {
+                fontSize: '0px'
+              }
+            }
+          }}
+          labelPosition={60}
+          segmentsStyle={{ transition: 'stroke .3s', cursor: 'pointer' }}
+          animate
+        />
+      </div>
       <input type="text" className="search" value={search} onChange={(e) => setSearch(e.target.value.toLowerCase())} placeholder="Søk etter champ..." />
       <div className="cardContainer">
         {cardList}
@@ -52,4 +79,29 @@ const App = () => {
   )
 }
 
+const getPiechartData = (champions: Champion[]) => {
+  const data = {
+    "top": 0,
+    "jungle": 0,
+    "mid": 0,
+    "bot": 0,
+    "support": 0
+  };
+
+  champions.forEach((champion) => {
+    if (champion.position) {
+      data[champion.position]++;
+    }
+  })
+
+  const pieData = [
+    { title: 'Top', value: data["top"], color: '#E38627' },
+    { title: 'Jungle', value: data["jungle"], color: '#C13C37' },
+    { title: 'Mid', value: data["mid"], color: '#18a459' },
+    { title: 'Bot', value: data["bot"], color: '#2b389c' },
+    { title: 'Support', value: data["support"], color: '#9a32a7' },
+  ];
+
+  return pieData;
+}
 export default App
